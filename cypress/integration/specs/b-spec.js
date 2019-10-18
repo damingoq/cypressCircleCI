bd7aeae41f90323e41c0aafc62f13f9e50e7e334
Daming Du <damingdu@Damings-MacBook-Pro.local>
2019-10-18 20:51:21 +0200

    Change the visit timeout

--- cypress/integration/specs/b-spec.js
+++ cypress/integration/specs/b-spec.js
@@ -2,7 +2,7 @@ import loginPo from "../pages/login.po";
 
 describe('b spec', function() {
     beforeEach(() =>  {
-        cy.visit('https://demo.cloud.oqton.ai')
+        cy.visit('https://demo.cloud.oqton.ai', 10000)
     });
 
     it('Login', function() {
